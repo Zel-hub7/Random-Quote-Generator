6fe86dda9fc1ed761cd9d3768dc508c7355def63
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-30 12:15:08 -0700

    ADD js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+//  Get quotes from API
+let apiQuotes = [];
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json()
+        console.log(apiQuotes)
+
+    } catch(error) {
+        
+    }
+}
+getQuotes();
