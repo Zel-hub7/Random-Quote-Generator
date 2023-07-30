9972e3c3ceb93e1dfa2f141ae34a21727f44ebc5
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-30 12:22:10 -0700

    Update the js file

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //  Get quotes from API
 let apiQuotes = [];
+
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote)
+}
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json()
-        console.log(apiQuotes)
-
+        newQuote()
     } catch(error) {
         
     }
