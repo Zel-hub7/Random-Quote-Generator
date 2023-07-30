2b8b075612982d024adb3f781b6a1320a4ccc4a6
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-30 12:31:42 -0700

    modify js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-//  Get quotes from API
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 function newQuote() {
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
+newQuoteBtn.addEventListener("click", newQuote);
 async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json()
-        newQuote()
-    } catch(error) {
-        
-    }
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
 }
 getQuotes();
